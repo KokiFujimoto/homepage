d8c8ac3de7ad8f888f74fa81bba9fb3b45f93402
Fujimoto <gegvp0330@gmail.com>
2022-06-21 12:47:21 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //
         if(\App::environment(['production'])) {
-            \URL::forcescheme('https');
+           \URL::forcescheme('https');
         }
     }
 }
