95e0a6563651a90fe5106f25b9df41044a0fba6f
Fujimoto <gegvp0330@gmail.com>
2022-06-21 11:31:39 +0900

    change maps

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
-    <script src="{{ asset('/js/maps.js') }}"></script>
     <script src="{{ mix('js/app.js') }}"></script>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
